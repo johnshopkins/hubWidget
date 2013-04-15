04f1d9514d0362dd2e12764c906e32d9673a333a
Jen Wachter <jwachter@jhu.edu>
2013-04-15 11:46:07 -0400

    Fixed loadHubWidget() calls.

--- js/custom/widget.js
+++ js/custom/widget.js
@@ -36,7 +36,7 @@ if (!jqExists("1.8.*")) {
 	};
 	
 	getScript("//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js", function() {
-		loadWidget();
+		loadHubWidget();
 	});
 	
 } else {
@@ -48,7 +48,7 @@ if (!jqExists("1.8.*")) {
 		});
 	});
 
-	loadWidget();
+	loadHubWidget();
 };
 
 function loadHubWidget() {
